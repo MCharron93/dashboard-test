0a96042d9a7803f4cef00a823c0fb7f41ad802fb
MCharron93 <michelle.charron93@gmail.com>
2021-02-12 15:49:50 -0700

    Refresh conatiner, colour palette edits

--- src/views/examples/Test.js
+++ src/views/examples/Test.js
@@ -14,64 +14,18 @@ import {
     InputGroup,
     // Row,
     Col,
+    Container,
 } from "reactstrap";
 
 const Test = () => {
     return (
         // NOTE these empty carrots are similar to the template parent div in Vue. They can be edited, but best practices note that these should be top level and never components of another kind. 
         <>
+            <Container>
             <Col lg="6" md="8">
-                <Card className="bg-neutral shadow border-0">
+                <Card className="bg-neutral shadow border-0 mt-5">
                     <CardHeader className="bg-transparent pb-5">
                         <div className="text-muted text-center mt-2 mb-4">
-                            <small>Snail cup width</small>
-                        </div>
-                        <div className="text-center">
-                            <Button
-                                className="btn-secondary btn-icon mr-4"
-                                color="default"
-                                href="#pablo"
-                                onClick={(e) => e.preventDefault()}
-                            >
-                                <span className="btn-inner--icon">
-                                    <img
-                                        alt="..."
-                                        src={
-                                            require("../../assets/img/icons/common/github.svg")
-                                                .default
-                                        }
-                                    />
-                                </span>
-                                <span className="btn-inner--text">Gothib</span>
-                            </Button>
-                            <Button
-                                className="btn-secondary btn-icon"
-                                color="default"
-                                href="#pablo"
-                                onClick={(e) => e.preventDefault()}
-                            >
-                                <span className="btn-inner--icon">
-                                    <img
-                                        alt="..."
-                                        src={
-                                            require("../../assets/img/icons/common/google.svg")
-                                                .default
-                                        }
-                                    />
-                                </span>
-                                <span className="btn-inner--text">Foogle</span>
-                            </Button>
-                        </div>
-                    </CardHeader>
-                    <CardBody className="px-lg-5 py-lg-5">
-                        <div className="text-center text-muted mb-4">
-                            <small>Here is some test data</small>
-                        </div>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardHeader>
-                        <div className="text-c">
                             Hello World
                         </div>
                     </CardHeader>
@@ -84,9 +38,17 @@ const Test = () => {
                             type="datetime-local">
                             </Input>
                         </InputGroup>
+                            <Button className="btn-3 btn-outline-success mt-2">Click me</Button>
                     </CardBody>
                 </Card>
+                <Card>
+                    <CardHeader>
+                        
+                    </CardHeader>
+                    
+                </Card>
             </Col>
+            </Container>
         </>
     );
 };
